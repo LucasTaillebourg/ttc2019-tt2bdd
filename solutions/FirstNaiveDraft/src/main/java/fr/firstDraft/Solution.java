a83111449a62fdaee00dcfecc5ecb6cf2ef4c0f4
Leah Lovelace <leahlovelace@linux.home>
2019-10-14 15:06:33 +0200

    skelton

--- solutions/FirstNaiveDraft/src/main/java/fr/firstDraft/Solution.java
+++ solutions/FirstNaiveDraft/src/main/java/fr/firstDraft/Solution.java
@@ -1,6 +1,9 @@
 package fr.firstDraft;
 
 public class Main {
+
+    private TruthTable inTable;
+    private BDD outBDD;
     public static void main(String[] args) {
 
     }
---
+++ solutions/FirstNaiveDraft/src/main/java/fr/firstDraft/TruthTableLoader.java
@@ -0,0 +1,6 @@
+package fr.firstDraft;
+
+public class TruthTableLoader {
+
+
+}
