49047219797227783aa46301a04d27c82a0bb08e
L.Taillebourg <lucas.taillebourg@gmail.com>
2019-10-28 14:40:08 +0100

    Debut

---
+++ solutions/FirstNaiveDraft/src/main/java/solution/Solution.java
@@ -0,0 +1,11 @@
+package solution;
+
+import ttc2019.metamodels.bdd.BDDPackage;
+import ttc2019.metamodels.tt.TTPackage;
+import ttc2019.metamodels.tt.TruthTable;
+
+public class Solution {
+    public static void main(String[] args) {
+      System.out.println("coucou");
+    }
+  }
