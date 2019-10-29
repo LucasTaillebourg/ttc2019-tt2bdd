e0f2779d14642ff09f70d13f13a0e3870801e3bf
L.Taillebourg <lucas.taillebourg@gmail.com>
2019-10-29 11:25:01 +0100

    Add medium transformation step :sparkles:

--- solutions/FirstNaiveDraft/src/ttc2019/Solution.java
+++ solutions/FirstNaiveDraft/src/ttc2019/Solution.java
@@ -44,6 +44,8 @@ public class Solution {
 
 		int depthOfTheTree = inputPortList.size();
 		int actualDepth = 0;
+
+		//Used for knowing the last iteration subtrees calculated
 		List<Subtree> lastLevelTree = new ArrayList<>();
 
 		//This is the tree generation, without any optimisation, so all branch are computed.
@@ -57,9 +59,7 @@ public class Solution {
 				Subtree subtree = bddFactory.createSubtree();
 
 				//Create the new port as a BDD type port
-				ttc2019.metamodels.bdd.InputPort bddPort = bddFactory.createInputPort();
-				bddPort.setOwner(bdd);
-				bddPort.setName(port.getName());
+				ttc2019.metamodels.bdd.InputPort bddPort = createPort(bddFactory, bdd, port);
 
 				//Set the tree port
 				subtree.setPort(bddPort);
@@ -69,15 +69,49 @@ public class Solution {
 
 				//We created one level of the tree, the first
 				actualDepth++;
-			} else if(actualDepth == depthOfTheTree){
+				//Adding to the last level deph tree for being able to use it at the next iteration.
+				lastLevelTree.add(subtree);
+			} else if(actualDepth == depthOfTheTree - 1 ){
 				// TODO Last level of the tree
 			} else {
-				// TODO general case, inside the tree when we create subTree
+				List<Subtree> nextLastLevelTree = new ArrayList<>();
+				for (Subtree subTree: lastLevelTree) {
+					//Create the new port as a BDD type port
+					ttc2019.metamodels.bdd.InputPort bddPort = createPort(bddFactory, bdd, port);
+
+					//Creating left tree
+					Subtree subLeftTree = bddFactory.createSubtree();
+					subLeftTree.setPort(bddPort);
+					subTree.setTreeForZero(subLeftTree);
+
+					//Creating right tree
+					Subtree subRightTree = bddFactory.createSubtree();
+					subRightTree.setPort(bddPort);
+					subTree.setTreeForZero(subRightTree);
+
+					//Adding the the next next level.
+					nextLastLevelTree.add(subLeftTree);
+					nextLastLevelTree.add(subRightTree);
+				}
+				//The new level tree for the next turn.
+				lastLevelTree = nextLastLevelTree;
+
+				//Another depth level created
+				actualDepth++;
 			}
 		}
 
+		//TODO Calculating the leaf value at the end of the tree, or maybe doing it in the last level iteration of the graph :thinking_face:s
+
 		//TODO optimized the tree with the reducing algorithm : https://www.cs.ox.ac.uk/people/james.worrell/lec5-2015.pdf
 
+	}
+
+	private InputPort createPort(BDDFactory bddFactory, BDD bdd, Port port) {
+		ttc2019.metamodels.bdd.InputPort bddPort = bddFactory.createInputPort();
+		bddPort.setOwner(bdd);
+		bddPort.setName(port.getName());
+		return bddPort;
 	}
 
 	public void save() throws IOException {
