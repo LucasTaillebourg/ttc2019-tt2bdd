8b09eb7627731f7b9654e9034a542cb8249c76ef
Lb-lea <happyvivus@gmail.com>
2019-11-13 16:45:07 +0100

    tree opti : took into acount having several output port

--- solutions/FirstNaiveDraft/src/ttc2019/TreeOptimization.java
+++ solutions/FirstNaiveDraft/src/ttc2019/TreeOptimization.java
@@ -3,6 +3,8 @@ package ttc2019;
 import ttc2019.metamodels.bdd.*;
 import ttc2019.metamodels.bdd.impl.LeafImpl;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,20 +23,39 @@ public class TreeOptimization {
     }
 
     /**
+     * Remove redundant tests
+     * @param bdd
+     * @return
+     */
+    private static BDD testReduction(BDD bdd){
+        for(Port port : bdd.getPorts()) {
+            if (port instanceof OutputPort) {
+
+            }
+        }
+        return  bdd;
+    }
+
+    /**
      * Remove duplicate leaves
      * @param bdd
      * @return
      */
     private static  BDD leafReduction(BDD bdd){
-        Leaf lastFalseLeaf = null;
-        Leaf lastTrueLeaf = null;
+        Leaf found;
+        List<Leaf> uniqueLeaves = new ArrayList<>();
         for(Port port : bdd.getPorts()){
             if(port instanceof  OutputPort){
               for(Assignment assignment:  ((OutputPort) port).getAssignments()){
-                  if(assignment.isValue()) {
-                      lastTrueLeaf = overrdeLeaf(assignment, lastTrueLeaf, true);
-                  }else{
-                      lastFalseLeaf = overrdeLeaf(assignment,lastFalseLeaf,false);
+                  if (!uniqueLeaves.contains(assignment.getOwner())) {
+                      found = getLeafIfContains(uniqueLeaves,assignment.getOwner());
+                      if(found !=null){
+                          assignment.getOwner().getOwnerSubtreeForZero().setTreeForZero(found);
+                          assignment.getOwner().getOwnerSubtreeForOne().setTreeForOne(found);
+                          assignment.setOwner(found);
+                      }else{
+                          uniqueLeaves.add(assignment.getOwner());
+                      }
                   }
               }
             }
@@ -42,17 +63,30 @@ public class TreeOptimization {
         return bdd;
     }
 
-    private static  Leaf overrdeLeaf(Assignment assignment, Leaf newLeaf,boolean branch){
-        if(newLeaf == null){
-            return assignment.getOwner();
+    private static Leaf getLeafIfContains(List<Leaf> leaves, Leaf leaf){
+        for(Leaf leafIt : leaves){
+            if (areLeafEquals(leafIt,leaf)){
+                return leafIt;
+            }
         }
-        Leaf oldLeaf = assignment.getOwner();
-        if(branch)
-            oldLeaf.getOwnerSubtreeForOne().setTreeForOne(newLeaf) ;
-        else
-            oldLeaf.getOwnerSubtreeForZero().setTreeForZero(newLeaf); ;
-        assignment.setOwner(newLeaf);
-        return newLeaf;
+        return null;
+    }
+
+    private static boolean areLeafEquals(Leaf leaf1, Leaf leaf2){
+        boolean found;
+        List<Port> falseStack =  leaf1.getAssignments().parallelStream().filter(Assignment::isValue).map(Assignment::getPort).collect(Collectors.toList());
+        List<Port> trueStack =  leaf1.getAssignments().parallelStream().filter(as -> !as.isValue()).map(Assignment::getPort).collect(Collectors.toList());
+        for(Assignment assign : leaf2.getAssignments()){
+            if(assign.isValue()){
+                found = trueStack.remove(assign.getPort());
+                if(!found) return false;
+            }else{
+                found = falseStack.remove(assign.getPort());
+                if(!found) return false;
+            }
+        }
+
+        return falseStack.isEmpty() && trueStack.isEmpty();
     }
 
 
